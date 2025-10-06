ab0e10ceb6564e4e5f8247b4d56b3aede92deb5f
Ian McLinden <mclinden.ian@gmail.com>
2025-10-06 10:25:47 -0500

    Switching from structopt to clap
    Signed-off-by: Ian McLinden <mclinden.ian@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
+use clap::Parser;
 use csscolorparser::Color;
 use rocket_notify::rocketchat::{Attachment, Client, Message};
 use std::{process, sync::LazyLock};
-use structopt::StructOpt;
 
 static HOSTNAME: LazyLock<String> = LazyLock::new(|| {
     hostname::get()
@@ -10,20 +10,20 @@ static HOSTNAME: LazyLock<String> = LazyLock::new(|| {
         .replace(".local", "")
 });
 
-#[derive(Debug, StructOpt)]
-#[structopt(about)]
+#[derive(Debug, Parser)]
+#[command(version, about, long_about, author)]
 struct Args {
     /// Rocket.Chat webhook URL
-    #[structopt(short, long, default_value, env = "ROCKET_NOTIFY_URL", hidden = true)]
-    url: String,
+    #[arg(short, long, env = "ROCKET_NOTIFY_URL", hide = true)]
+    url: Option<String>,
 
     /// Alias for the message sender
-    #[structopt(short, long, default_value = &HOSTNAME, env = "ROCKET_NOTIFY_ALIAS")]
+    #[arg(short, long, default_value_t = HOSTNAME.clone(), env = "ROCKET_NOTIFY_ALIAS")]
     alias: String,
 
     /// Set the sender's icon to an emoji
-    #[structopt(
-        short = "i",
+    #[arg(
+        short = 'i',
         long = "icon",
         default_value = ":computer:",
         env = "ROCKET_NOTIFY_ICON"
@@ -31,27 +31,27 @@ struct Args {
     emoji: String,
 
     /// Set the sender's icon to the provided URL (replaces -i,--icon)
-    #[structopt(short = "A", long = "avatar", env = "ROCKET_NOTIFY_AVATAR")]
+    #[arg(short = 'A', long = "avatar", env = "ROCKET_NOTIFY_AVATAR")]
     avatar: Option<String>,
 
     /// Title of the message
-    #[structopt(short, long, default_value, env = "ROCKET_NOTIFY_TITLE")]
+    #[arg(short, long, default_value = "", env = "ROCKET_NOTIFY_TITLE")]
     title: String,
 
     /// Color of the message header
-    #[structopt(short, long, default_value = "darkgrey", env = "ROCKET_NOTIFY_COLOR")]
+    #[arg(short, long, default_value = "darkgrey", env = "ROCKET_NOTIFY_COLOR")]
     color: Color,
 
     /// Send with message block collapsed
-    #[structopt(short, long, env = "ROCKET_NOTIFY_MINIMIZE")]
+    #[arg(short, long, env = "ROCKET_NOTIFY_MINIMIZE")]
     minimize: bool,
 
     /// Channel to which the mesage will be sent, like '#general' or '@eric'
-    #[structopt(env = "ROCKET_NOTIFY_CHANNEL")]
+    #[arg(env = "ROCKET_NOTIFY_CHANNEL")]
     channel: String,
 
     /// Message to send
-    #[structopt(env = "ROCKET_NOTIFY_MESSAGE")]
+    #[arg(env = "ROCKET_NOTIFY_MESSAGE")]
     message: String,
 }
 
@@ -64,14 +64,14 @@ fn print_success<S: std::fmt::Display>(msg: S) {
 }
 
 fn main() {
-    let args = Args::from_args();
+    let args = Args::parse();
 
-    if args.url.is_empty() {
+    let Some(url) = args.url else {
         print_err(
             "ROCKET_NOTIFY_URL not set.\nPlease acquire a webhook url from an admin, and then\n  `export ROCKET_NOTIFY_URL=https://{webhookURL}`",
         );
         process::exit(1);
-    }
+    };
 
     let mut message = Message::new()
         .channel(&args.channel)
@@ -91,7 +91,7 @@ fn main() {
         message.emoji(args.emoji)
     };
 
-    match Client::new(&args.url).post_message(&message) {
+    match Client::new(&url).post_message(&message) {
         Err(e) => {
             print_err(e);
             process::exit(1);
